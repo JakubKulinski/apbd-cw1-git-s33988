31d9fc80f4132543b1c3c4d06306a46e83e79d46
Jakub Kuliński <s33988@pjwstk.edu.pl>
2026-03-17 16:48:49 +0100

    Implement minimum value calculation

--- apbd-cw1-git-s33988-app/StatisticsHelper.cs
+++ apbd-cw1-git-s33988-app/StatisticsHelper.cs
@@ -39,6 +39,16 @@
     
     public static int CalculateMin(int[] values)
     {
-        return values[0];
+        int min = values[0];
+
+        for (int i = 1; i < values.Length; i++)
+        {
+            if (values[i] < min)
+            {
+                min = values[i];
+            }
+        }
+
+        return min;
     }
 }
