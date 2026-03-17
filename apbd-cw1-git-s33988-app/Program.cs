899d0e9b111c8488a2b50d307a32f89356e54002
Jakub Kuliński <s33988@pjwstk.edu.pl>
2026-03-17 15:56:44 +0100

    Add average calculation

--- apbd-cw1-git-s33988-app/Program.cs
+++ apbd-cw1-git-s33988-app/Program.cs
@@ -4,4 +4,11 @@ string firstInput = Console.ReadLine();
 Console.WriteLine("Enter second number:");
 string secondInput = Console.ReadLine();
 
-Console.WriteLine($"You entered: {firstInput} and {secondInput}");
+int firstNumber = int.Parse(firstInput);
+int secondNumber = int.Parse(secondInput);
+
+int sum = StatisticsHelper.Sum(firstNumber, secondNumber);
+double average = StatisticsHelper.Average(firstNumber, secondNumber);
+
+Console.WriteLine($"Sum: {sum}");
+Console.WriteLine($"Average: {average}");
--- apbd-cw1-git-s33988-app/StatisticsHelper.cs
+++ apbd-cw1-git-s33988-app/StatisticsHelper.cs
@@ -1,9 +1,12 @@
-﻿namespace apbd_cw1_git_s33988_app;
-
-public static class StatisticsHelper
+﻿public static class StatisticsHelper
 {
     public static int Sum(int a, int b)
     {
         return a + b;
+    }
+    
+    public static double Average(int a, int b)
+    {
+        return (a + b) / 2.0;
     }
 }
