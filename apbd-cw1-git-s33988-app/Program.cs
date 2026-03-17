80697b2351eb79dba9329afbb8512b23513edbe4
Jakub Kuliński <s33988@pjwstk.edu.pl>
2026-03-17 15:48:04 +0100

    Add basic user input

--- apbd-cw1-git-s33988-app/Program.cs
+++ apbd-cw1-git-s33988-app/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter first number:");
+string firstInput = Console.ReadLine();
+
+Console.WriteLine("Enter second number:");
+string secondInput = Console.ReadLine();
+
+Console.WriteLine($"You entered: {firstInput} and {secondInput}");
