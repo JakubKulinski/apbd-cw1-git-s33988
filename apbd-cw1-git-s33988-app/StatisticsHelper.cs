fb04047580705a7e4ae7ebd252718a6ab2c4250a
Jakub Kuliński <s33988@pjwstk.edu.pl>
2026-03-17 16:12:32 +0100

    Add CalculateAverage method

--- apbd-cw1-git-s33988-app/StatisticsHelper.cs
+++ apbd-cw1-git-s33988-app/StatisticsHelper.cs
@@ -9,4 +9,16 @@
     {
         return (a + b) / 2.0;
     }
+    
+    public static double CalculateAverage(int[] values)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            sum += values[i];
+        }
+
+        return (double)sum / values.Length;
+    }
 }
